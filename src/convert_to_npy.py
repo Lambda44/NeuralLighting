cfc422e51d64b052c2b0fd6c121147cb54cd0a0e
Beodrich <60353906+Beodrich@users.noreply.github.com>
2021-05-18 16:32:50 -0500

    got converting to .npy working

--- src/convert_to_npy.py
+++ src/convert_to_npy.py
@@ -1,20 +1,26 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np #the funny statment 
 from PIL import Image
 import os
 #for saving the numpy array as a csv
 import pandas as pd
 
-directory = "/new_data/kuan-yu/2048/"
-outputPath = "new_data/kuan-yu/output"
+directory = "new_data/kuan-yu/subset/"
+outputPath = "new_data/kuan-yu/output/"
 
 imageList= os.listdir(directory)
-assert (len(imageList)>0, "Images not loaded in correctly")
+assert len(imageList)>0, "Images not loaded in correctly"
+
 for current in imageList:
     if current.endswith(".jpg") or current.endswith(".png"):
         # convert the image to numpy array
-        a= Image.open(current)
+        print(directory+current)
+        a= Image.open(directory+ current)
         data= np.array(a)
-        pd.DataFrame(data).to_csv(outputPath) #the output 
+        data.tofile(outputPath+'data.csv',sep=', ')
+       # df= pd.DataFrame(data)
+       # pd.DataFrame(data).to_csv(outputPath+"test.csv") #the output 
    
 
 
