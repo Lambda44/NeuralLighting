ae1d7954d509a0cef669410d4e52df771440aece
Beodrich <60353906+Beodrich@users.noreply.github.com>
2021-05-20 16:03:57 -0500

    Update convert_to_npy.py

--- src/convert_to_npy.py
+++ src/convert_to_npy.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 import numpy as np #the funny statment 
 from PIL import Image
@@ -6,21 +5,28 @@ import os
 #for saving the numpy array as a csv
 import pandas as pd
 
-directory = "new_data/kuan-yu/2048/"
-outputPath = "new_data/kuan-yu/output/"
-count=0
-imageList= os.listdir(directory)
-assert len(imageList)>0, "Images not loaded in correctly"
 
-for current in imageList:
-    if current.endswith(".jpg") or current.endswith(".png"):
-        # convert the image to numpy array
+def convertImagesToNpy(directory, outputPath):
+    count=0
+    imageList= os.listdir(directory)
+    assert len(imageList)>0, "Images not loaded in correctly"
+
+    for current in imageList:
         print(directory+current)
         a= Image.open(directory+ current)
         data= np.array(a)
         path= outputPath+ 'image_'+str(count)+'.npy'
         data.tofile(str(path),sep=', ')
         count+=1
+
+
+
+
+directory = "new_data/kuan-yu/2048/"
+outputPath = "new_data/kuan-yu/output/"
+convertImagesToNpy(directory,outputPath)
+
+
 
 
 
