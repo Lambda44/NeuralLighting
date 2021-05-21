5d86c3b6e14f612b1a0e0c1a222e9d6c2ba02c04
Lambda44 <brendanlava@gmail.com>
2021-05-21 13:08:01 -0500

    Update convert_to_npy.py

--- src/convert_to_npy.py
+++ src/convert_to_npy.py
@@ -19,13 +19,43 @@ def convertImagesToNpy(directory, outputPath):
         data.tofile(str(path),sep=', ')
         count+=1
 
+def convertBasisToNpy(directory, outputPath, renderMode):
+    count=0
+    imageList= os.listdir(directory)
+    assert len(imageList)>0, "Images not loaded in correctly"
 
+    for current in imageList:
+        print(directory+current)
+        a= Image.open(directory+ current)
+        data= np.array(a)
+        path= outputPath+ 'basis'+str(renderMode)+'_'+str(count)+'.npy'
+        data.tofile(str(path),sep=', ')
+        count+=1
 
 
 directory = "new_data/kuan-yu/2048/"
-outputPath = "new_data/kuan-yu/output/"
-convertImagesToNpy(directory,outputPath)
+outputPath = "new_data/kuan-yu/output/image"
+#convertImagesToNpy(directory,outputPath)
 
+directory = "new_data/kuan-yu/blender/lambert/"
+outputPath = "new_data/kuan-yu/output/basis"
+convertBasisToNpy(directory, outputPath, 0)
+
+directory = "new_data/kuan-yu/blender/ct_02/"
+outputPath = "new_data/kuan-yu/output/basis"
+convertBasisToNpy(directory, outputPath, 1)
+
+directory = "new_data/kuan-yu/blender/ct_05/"
+outputPath = "new_data/kuan-yu/output/basis"
+convertBasisToNpy(directory, outputPath, 2)
+
+directory = "new_data/kuan-yu/blender/ct_13/"
+outputPath = "new_data/kuan-yu/output/basis"
+convertBasisToNpy(directory, outputPath, 3)
+
+directory = "new_data/kuan-yu/blender/ct_34/"
+outputPath = "new_data/kuan-yu/output/basis"
+convertBasisToNpy(directory, outputPath, 4)
 
 
 
